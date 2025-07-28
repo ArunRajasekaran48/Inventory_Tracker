e11072d999d36e5bdda0fb09fc64d02f44176805
ArunRajasekaran48 <arun.r2023cse@sece.ac.in>
2025-07-28 10:49:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors({origin:"https://inventory-tracker-sigma-eight.vercel.app"}));
+app.use(cors({
+  origin: "https://inventory-tracker-sigma-eight.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
