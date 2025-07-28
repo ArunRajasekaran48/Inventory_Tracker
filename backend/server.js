478dbb98539b2972cfb156251414b65715559631
ArunRajasekaran48 <arun.r2023cse@sece.ac.in>
2025-07-28 11:17:14 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://inventory-tracker-sigma-eight.vercel.app",
+  origin: "https://inventory-tracker-ten-eta.vercel.app",
   credentials: true
 }));
 app.use(express.json());
